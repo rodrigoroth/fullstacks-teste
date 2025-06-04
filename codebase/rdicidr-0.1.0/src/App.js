ae7b7f554719df5bac92c6612dbd769faa4de9fd
Rodrigo Roth <rodrigo1992ster@gmail.com>
2025-06-04 15:50:52 -0300

    app.js: forcing lint error

--- codebase/rdicidr-0.1.0/src/App.js
+++ codebase/rdicidr-0.1.0/src/App.js
@@ -1,34 +1,37 @@
-import IPv4Addr from "./IPv4Addr";
-import "./App.css";
+ import IPv4Addr from "./IPv4Addr";
+ import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div>CIDR Calculator</div>
-      </header>
-      <IPv4Addr />
-      <footer>
-        <hr />
-        If you find this tool useful, you might enjoy reading my blog:{" "}
-        <a href="https://rderik.com">rderik.com</a>
-      </footer>
-    </div>
-  );
-}
+ function App() {
+-  return (
+-    <div className="App">
+-      <header className="App-header">
+-        <div>CIDR Calculator</div>
+-      </header>
+-      <IPv4Addr />
+-      <footer>
+-        <hr />
+-        If you find this tool useful, you might enjoy reading my blog:{ " " }
+-        <a href="https://rderik.com">rderik.com</a>
+-      </footer>
+-    </div>
+-  );
+-}
++  // ⬇ aqui eu crio uma variável sem usar:
++  const variavelNaoUsada = 42;
 
-  import React from "react";
-
-  function App() {
-+   const foo = 123;   // ← Variável declarada e não utilizada
-
-    console.log("Hello World");  // (Exemplo de console no final da linha com ;)
-    return (
-      <div>Oi</div>
-    );
-  }
-
-  export default App;
-
-
-export default App;
++  return (
++    <div className="App">
++      <header className="App-header">
++        <div>CIDR Calculator</div>
++      </header>
++      <IPv4Addr />
++      <footer>
++        <hr />
++        If you find this tool useful, you might enjoy reading my blog:{ " " }
++        <a href="https://rderik.com">rderik.com</a>
++      </footer>
++    </div>
++  );
++}
+ 
+ export default App;
